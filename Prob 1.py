581800712eda0c5ff2e5c0ee91f3ea962b4c92ba
RiteshSutrave7 <159562012+RiteshSutrave7@users.noreply.github.com>
2026-02-16 11:29:45 +0530

    Delete Prob 1.py

--- Prob 1.py
+++
@@ -1,8 +0,0 @@
-# Unique Words in a Sentence   
-sentence = "python is easy and python is powerful"
-words = sentence.split()
-unique_words = set(words)
-
-print(f"Unique words count: {len(unique_words)}")
-print(f"Unique words: {unique_words}")
-                                                                    
