5a7214e5d429c7e5d170f0297cc4757994d76a30
RiteshSutrave7 <159562012+RiteshSutrave7@users.noreply.github.com>
2026-02-16 11:32:35 +0530

    Delete Prob 9.py

--- Prob 9.py
+++
@@ -1,6 +0,0 @@
-# 9 Check if a Key Exists in a Dictionary
-user_db = {"id": 1, "username": "dev_user"}
-
-# The 'in' keyword checks if a key exists in the dictionary keys
-if "username" in user_db:
-    print("Employee exists")
