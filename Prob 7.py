ec61e6d3405d34ca9b527a9fb1c04abbe25fa9cb
RiteshSutrave7 <159562012+RiteshSutrave7@users.noreply.github.com>
2026-02-16 11:30:47 +0530

    Delete Prob 7.py

--- Prob 7.py
+++
@@ -1,9 +0,0 @@
-# 7 Count Character Frequency 
-text = "pythonp"
-freq = {}
-
-for char in text:
-    # .get(char, 0) looks for the char; if not found, it starts at 0
-    freq[char] = freq.get(char, 0) + 1
-
-print(freq)
