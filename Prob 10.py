8a53dfbf3bcb9055f3cd16584cc26b37a6bc2b41
RiteshSutrave7 <159562012+RiteshSutrave7@users.noreply.github.com>
2026-02-16 11:32:17 +0530

    Delete Prob 10.py

--- Prob 10.py
+++
@@ -1,7 +0,0 @@
-# 10 Calculate Average Marks 
-marks = [80, 75, 60, 75]
-
-# sum() adds all numbers, len() counts how many numbers there are
-average = sum(marks) / len(marks)
-
-print(f"Average Marks: {average}")
